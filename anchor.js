0aeb895aa6353cfc97f848d7af4daf6e116b40e4
Geronymos <spielerbereich@gmail.com>
2020-09-25 20:06:21 +0200

    anchor animation + works on hard reload

--- anchor.js
+++ anchor.js
@@ -1,15 +1,23 @@
 // Registering component in foo-component.js
 AFRAME.registerComponent('anchor', {
     schema: {
-        property: { type: 'string' }
+        property: { type: 'string' },
+        delay: { type: 'int', default: 0 },
     },
     init: function () {
-
+        const {property, delay} = this.data;
+        // this.setProperty(property, 0);
+        this.el.setAttribute(property, location.hash);
     },
     update: function () {
-        window.onhashchange = () => {
-            this.el.setAttribute(this.data.property, location.hash)
-        }
+        const {property, delay} = this.data;
+        window.onhashchange = () => this.setProperty(property, delay);
+    },
+    setProperty: function (property, delay) {
+        this.el.emit("fade");
+        window.setTimeout(() => {
+            this.el.setAttribute(property, location.hash);
+        }, delay)
     },
     tick: function () { },
     remove: function () { },
