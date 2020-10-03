abaa03f8da7a517bde3eafbaab5307cbc872b1c3
Geronymos <spielerbereich@gmail.com>
2020-10-03 19:06:15 +0200

    change link and visibility on room change

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 // Registering component in foo-component.js
 AFRAME.registerComponent('portal', {
     schema: {
-        from: { type: 'string', default: '#' },
+        domain: { type: 'selector', default: '#image-360' },
+        from: { type: 'asset', default: '#' },
         to: { type: 'asset', default: '#' },
         title: { type: 'string', default: '#' }
     },
     init: function () {
-        const link = document.createElement("a-link");
         console.log(this);
-        link.setAttribute('link',{
+        this.link = document.createElement("a-link");
+        this.link.className = 'link';
+        this.el.append(this.link);
+    },
+
+    update: function () {
+        this.link.setAttribute('link', {
             title: this.data.title,
-            href: `#${this.data.to.id}`,
+            href: `#${this.data.to}`,
             image: this.data.to
         });
-        this.el.append(link);
-     },
-    update: function () { 
     },
-    tick: function () { },
+    tick: function () {
+        const domain_image = this.data.domain.components.material.data.src;
+        const is_from_in_scene = this.data.from == domain_image;
+        const is_to_in_scene = this.data.to == domain_image;
+        const is_in_scene = is_from_in_scene || is_to_in_scene;
+
+        // console.log(domain_image, this.data.from)
+
+        this.el.object3D.visible = is_in_scene;
+
+        this.link.setAttribute('link', {
+            title: this.data.title,
+            href: `#${is_to_in_scene ? this.data.from.id : this.data.to.id}`,
+            image: is_to_in_scene ? this.data.from : this.data.to
+        });
+     },
     remove: function () { },
     pause: function () { },
     play: function () { }
